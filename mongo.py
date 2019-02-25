d42ccaf4ffe4baf6049a13cb7b4dc901e78d3221
Rahul Malhotra <r_malhot@encs.concordia.ca>
2019-02-24 21:32:43 -0500

    First Commit

--- mongo.py
+++ mongo.py
@@ -70,6 +70,21 @@ mydoc = mycol.find(myquery)
 
 for x in mydoc:
   print(x)
+mydoc = mycol.find().sort("name")
+
+for x in mydoc:
+  print(x)
+mycol = mydb["customers"]
+
+mycol.drop()
+myquery = { "address": "Valley 345" }
+newvalues = { "$set": { "address": "Canyon 123" } }
+
+mycol.update_one(myquery, newvalues)
+
+#print "customers" after the update:
+for x in mycol.find():
+  print(x)
 """dblist = myclient.list_database_names()
 collist = mydb.list_collection_names()
 print(dblist)
